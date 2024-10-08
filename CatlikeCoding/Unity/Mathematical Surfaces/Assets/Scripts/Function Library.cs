a1cd3f1b9eef4a940dc6373fcd41d218a022b9a4
meursw <filippos-str@hotmail.gr>
2024-10-09 00:20:16 +0300

    unity progress

--- CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Function Library.cs
+++ CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Function Library.cs
@@ -3,27 +3,49 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
     public delegate Vector3 Function (float u, float v, float t);
-    static Function[] functions = {Wave, MultiWave, Ripple};
+    static Function[] functions = {Wave, MultiWave, Ripple, Sphere};
 
-    public enum FunctionName {Wave, MultiWave, Ripple};
+    public enum FunctionName {Wave, MultiWave, Ripple, Sphere};
 
     public static Function GetFunction(FunctionName name) {
         return functions[(int)name];
     }
 
-    public static Vector3 Wave(float u, float v, float t) {
-        Vector3 p = Vector3.zero;
-        p.x = u;
-        return p;
-    }
+    public static Vector3 Wave (float u, float v, float t) {
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + v + t));
+		p.z = v;
+		return p;
+	}
 
-    public static Vector3 MultiWave(float u, float v, float t) {
-        Vector3 p = Vector3.zero;
-        return p;
-    }
+    public static Vector3 MultiWave (float u, float v, float t) {
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + 0.5f * t));
+		p.y += 0.5f * Sin(2f * PI * (v + t));
+		p.y += Sin(PI * (u + v + 0.25f * t));
+		p.y *= 1f / 2.5f;
+		p.z = v;
+		return p;
+	}
 
-    public static Vector3 Ripple(float u, float v, float t) {
-        Vector3 p = Vector3.zero;
+	public static Vector3 Ripple (float u, float v, float t) {
+		float d = Sqrt(u * u + v * v);
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (4f * d - t));
+		p.y /= 1f + 10f * d;
+		p.z = v;
+		return p;
+	}
+
+    public static Vector3 Sphere(float u, float v, float t) {
+        float r = Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = r * Sin(PI * u);
+        p.y = Sin(PI * 0.5f * v);
+        p.z = r * Cos(PI * u);
         return p;
     }
 }
--- CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Graph.cs
+++ CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Graph.cs
@@ -15,37 +15,30 @@ public class Graph: MonoBehaviour {
 
     void Awake() {
         float step = 2f / resolution;
-        var position = Vector3.zero;
 		var scale = Vector3.one * step;
 
         points = new Transform[resolution * resolution];
-		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-            if(x == resolution) {
-                x = 0;
-                z++;
-            }
+		for (int i = 0; i < points.Length; i++) {
 			Transform point = points[i] = Instantiate(pointPrefab);
-
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-			point.localPosition = position;
 			point.localScale = scale;
-
             point.SetParent(transform, false);
 		}
     }
 
     void Update() {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-
         float time = Time.time;
-        for(int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
+        float step = 2f / resolution;
 
-            position = f(position.x, position.z, time);
-
-            point.localPosition = position;
+        float v = 0.5f * step - 1.0f;
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if(x == resolution) {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1.0f;
+            }
+            float u = (x + 0.5f) * step - 1.0f;
+            points[i].localPosition = f(u, v, time);
         }
         
     }
