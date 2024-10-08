1d2ed01cefb0c00101445b54c280e3bd4d156052
meursw <filippos-str@hotmail.gr>
2024-10-09 00:30:25 +0300

    finished math surf tut for unity

--- CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Function Library.cs
+++ CatlikeCoding/Unity/Mathematical Surfaces/Assets/Scripts/Function Library.cs
@@ -3,9 +3,9 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
     public delegate Vector3 Function (float u, float v, float t);
-    static Function[] functions = {Wave, MultiWave, Ripple, Sphere};
+    static Function[] functions = {Wave, MultiWave, Ripple, Sphere, Torus};
 
-    public enum FunctionName {Wave, MultiWave, Ripple, Sphere};
+    public enum FunctionName {Wave, MultiWave, Ripple, Sphere, Torus};
 
     public static Function GetFunction(FunctionName name) {
         return functions[(int)name];
@@ -41,11 +41,24 @@ public static class FunctionLibrary {
 	}
 
     public static Vector3 Sphere(float u, float v, float t) {
-        float r = Cos(0.5f * PI * v);
-        Vector3 p;
-        p.x = r * Sin(PI * u);
-        p.y = Sin(PI * 0.5f * v);
-        p.z = r * Cos(PI * u);
+		float r = 0.9f + 0.1f * Sin(PI * (10f * u + 5f * v + t));
+        float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+
         return p;
     }
+
+    public static Vector3 Torus (float u, float v, float t) {
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
 }
