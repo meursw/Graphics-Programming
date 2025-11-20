f6e945b1a1bdd56635f925aa26dc727068b50b36
meursw <118079913+meursw@users.noreply.github.com>
2025-11-21 01:12:11 +0200

    Fractal start

--- CatlikeCoding/Unity/Basics/Assets/Fractal/Scripts/Fractal.cs
+++ CatlikeCoding/Unity/Basics/Assets/Fractal/Scripts/Fractal.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
-public class Fractal: MonoBehaviour {
-    [SerializeField, Range(1, 8)]
+public class Fractal : MonoBehaviour {
+    [SerializeField, Range(1,8)]
     int depth = 4;
 
     void Start() {
         name = "Fractal " + depth;
-        if(depth <= 1)
+        
+        if(depth <= 1) {
             return;
+        }
         
         Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
-		Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-		Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
         Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
         Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
+        
 
         childA.transform.SetParent(transform, false);
         childB.transform.SetParent(transform, false);
@@ -27,13 +30,13 @@ public class Fractal: MonoBehaviour {
         transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
     }
 
-    Fractal CreateChild (Vector3 direction, Quaternion rotation) {
-		Fractal child = Instantiate(this);
-		child.depth = depth - 1;
-		child.transform.localPosition = 0.75f * direction;
-		child.transform.localRotation = rotation;
-		child.transform.localScale = 0.5f * Vector3.one;
-		return child;
-	}
+    Fractal CreateChild(Vector3 direction, Quaternion rotation) {
+        Fractal child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.localPosition = 0.75f * direction;
+        child.transform.localRotation = rotation;
+        child.transform.localScale= 0.5f * Vector3.one;
+        return child;
+    }
 
 }
