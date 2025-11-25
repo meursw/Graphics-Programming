6775feac2511ef846007e79d37a1371f9476127b
meursw <118079913+meursw@users.noreply.github.com>
2025-11-26 00:45:17 +0200

    DX11 3rd Tut

--- DirectX 11/3. Initalizing DX11/applicationclass.cpp
+++ DirectX 11/3. Initalizing DX11/applicationclass.cpp
@@ -1,49 +1,26 @@
-////////////////////////////////////////////////////////////////////////////////
-// Filename: applicationclass.cpp
-////////////////////////////////////////////////////////////////////////////////
 #include "applicationclass.h"
+#
+#include <iostream>
+#include <fstream>
 
 
-ApplicationClass::ApplicationClass()
-{
-	m_Direct3D = 0;
-}
+ApplicationClass::ApplicationClass() { m_Direct3D = 0; }
+ApplicationClass::ApplicationClass(const ApplicationClass& o) {}
+ApplicationClass::~ApplicationClass() {}
 
-
-ApplicationClass::ApplicationClass(const ApplicationClass& other)
-{
-}
-
-
-ApplicationClass::~ApplicationClass()
-{
-}
-
-
-bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
-{
-	bool result;
-
-
-	// Create and initialize the Direct3D object.
+bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd) {
 	m_Direct3D = new D3DClass;
 
-	result = m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
-	if(!result)
-	{
-		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+	if (!m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR)) {
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"ERROR", MB_OK);
 		return false;
 	}
 
 	return true;
 }
 
-
-void ApplicationClass::Shutdown()
-{
-	// Release the Direct3D object.
-	if(m_Direct3D)
-	{
+void ApplicationClass::Shutdown() {
+	if (m_Direct3D) {
 		m_Direct3D->Shutdown();
 		delete m_Direct3D;
 		m_Direct3D = 0;
@@ -53,30 +30,29 @@ void ApplicationClass::Shutdown()
 }
 
 
-bool ApplicationClass::Frame()
-{
-	bool result;
-
-
-	// Render the graphics scene.
-	result = Render();
-	if(!result)
-	{
-		return false;
-	}
-
-	return true;
+bool ApplicationClass::Frame() {
+	return Render();
 }
 
 
-bool ApplicationClass::Render()
-{
-	// Clear the buffers to begin the scene.
-	m_Direct3D->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
+bool ApplicationClass::Render() {
+	m_Direct3D->BeginScene(0.5f, 0.5f, 0.5f, 1.0);
 
 
-	// Present the rendered scene to the screen.
 	m_Direct3D->EndScene();
-
 	return true;
+}
+
+void ApplicationClass::writetofile() {
+	std::ofstream file("vci.txt", std::ios::app);
+
+	char buf[128];
+	int mem;
+
+	m_Direct3D->GetVideoCardInfo(buf, mem);
+
+	file << buf << std::endl;
+	file << mem << std::endl;
+
+	file.close();
 }
--- DirectX 11/3. Initalizing DX11/applicationclass.h
+++ DirectX 11/3. Initalizing DX11/applicationclass.h
@@ -1,30 +1,15 @@
-////////////////////////////////////////////////////////////////////////////////
-// Filename: applicationclass.h
-////////////////////////////////////////////////////////////////////////////////
-#ifndef _APPLICATIONCLASS_H_
-#define _APPLICATIONCLASS_H_
+#pragma once
 
-
-///////////////////////
-// MY CLASS INCLUDES //
-///////////////////////
 #include "d3dclass.h"
 
 
-/////////////
-// GLOBALS //
-/////////////
+// GLOBALS
 const bool FULL_SCREEN = false;
-const bool VSYNC_ENABLED = true;
+const bool VSYNC_ENABLED = false;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.3f;
 
-
-////////////////////////////////////////////////////////////////////////////////
-// Class name: ApplicationClass
-////////////////////////////////////////////////////////////////////////////////
-class ApplicationClass
-{
+class ApplicationClass {
 public:
 	ApplicationClass();
 	ApplicationClass(const ApplicationClass&);
@@ -34,11 +19,12 @@ public:
 	void Shutdown();
 	bool Frame();
 
+	void writetofile();
+
 private:
 	bool Render();
 
-private:
+public:
 	D3DClass* m_Direct3D;
-};
 
-#endif
+};
