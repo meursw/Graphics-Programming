e0acbfa6977de3e15c37296ba35b5a727c916a48
meursw <118079913+meursw@users.noreply.github.com>
2025-12-14 03:30:35 +0200

    DX11 Specular Lighting

---
+++ DirectX 11/10. Specular Lighting/applicationclass.cpp
@@ -0,0 +1,183 @@
+#include "applicationclass.h"
+
+
+ApplicationClass::ApplicationClass() { 
+	m_Direct3D = 0; 
+	m_Camera = 0;
+	m_Model = 0;
+	m_ColorShader = 0;
+	m_TextureShader = 0;
+	m_LightShader = 0;
+	m_Light = 0;
+}
+ApplicationClass::ApplicationClass(const ApplicationClass& o) {}
+ApplicationClass::~ApplicationClass() {}
+
+bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd) {
+	char modelFilename[128];
+	char textureFilename[128];
+
+	// Create and initialize the Direct3D object.
+	m_Direct3D = new D3DClass;
+
+	if (!m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR)) {
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"ERROR", MB_OK);
+		return false;
+	}
+
+	// Create camera object
+	m_Camera = new CameraClass;
+	m_Camera->SetPosition(0.0f, 1.0f, -4.0f);
+
+
+	// Create model class
+	m_Model = new ModelClass;
+
+	// Set the file name of the texture.
+	strcpy_s(textureFilename, "../DX11/Assets/Textures/mri.png");
+	
+	// Set the file name of the model.
+	strcpy_s(modelFilename, "../DX11/Assets/Models/mri.txt");
+	
+	if (!m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), modelFilename, textureFilename)) {
+		MessageBox(hwnd, L"Could not initialize the model object.", L"Error", MB_OK);
+		return false;
+	}
+
+	//Create color shader object
+	/*m_ColorShader = new ColorShaderClass;
+	if (!m_ColorShader->Initialize(m_Direct3D->GetDevice(), hwnd)) {
+		MessageBox(hwnd, L"Could not initialize the color shader object.", L"Error", MB_OK);
+		return false;
+	}*/
+
+	// Create and initialize the texture shader object.
+	/*m_TextureShader = new TextureShaderClass;
+
+	if (!m_TextureShader->Initialize(m_Direct3D->GetDevice(), hwnd))
+	{
+		MessageBox(hwnd, L"Could not initialize the texture shader object.", L"Error", MB_OK);
+		return false;
+	}*/
+
+	// Create and initialize the light shader object
+	m_LightShader = new LightShaderClass;
+	if (!m_LightShader->Initialize(m_Direct3D->GetDevice(), hwnd)) {
+		MessageBox(hwnd, L"Could not initialize the light shader object.", L"Error", MB_OK);
+		return false;
+	}
+
+	m_Light = new LightClass;
+	m_Light->SetAmbientColor(0.15f, 0.15f, 0.15f, 1.0f);
+	m_Light->SetDiffuseColor(1.0f, 0.0f, 0.0f, 1.0f);
+	m_Light->SetDirection(1.0f, 0.0f, 1.0f);
+	m_Light->SetSpecularColor(0.0f, 1.0f, 0.0f, 1.0f);
+	m_Light->SetSpecularPower(8.0f);
+
+	return true;
+}
+
+void ApplicationClass::Shutdown() {
+	if (m_Direct3D) {
+		m_Direct3D->Shutdown();
+		delete m_Direct3D;
+		m_Direct3D = 0;
+	}
+
+	// Release the light object.
+	if (m_Light)
+	{
+		delete m_Light;
+		m_Light = 0;
+	}
+
+	// Release the light shader object.
+	if (m_LightShader)
+	{
+		m_LightShader->Shutdown();
+		delete m_LightShader;
+		m_LightShader = 0;
+	}
+
+	// Release the color shader object.
+	if (m_ColorShader)
+	{
+		m_ColorShader->Shutdown();
+		delete m_ColorShader;
+		m_ColorShader = 0;
+	}
+
+	// Release the texture shader object.
+	if (m_TextureShader)
+	{
+		m_TextureShader->Shutdown();
+		delete m_TextureShader;
+		m_TextureShader = 0;
+	}
+
+	// Release the model object.
+	if (m_Model)
+	{
+		m_Model->Shutdown();
+		delete m_Model;
+		m_Model = 0;
+	}
+
+	// Release the camera object.
+	if (m_Camera)
+	{
+		delete m_Camera;
+		m_Camera = 0;
+	}
+
+	return;
+}
+
+
+bool ApplicationClass::Frame() {
+	static float rotation = 0.0f;
+
+	rotation -= 0.0174532925f * 0.8f;
+	if (rotation < 0.0f) {
+		rotation += 360.0f;
+	}
+
+	return Render(rotation);
+}
+
+
+bool ApplicationClass::Render(float rotation) {
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+	XMMATRIX scaleMatrix, rotateMatrix, translateMatrix, srMatrix;
+	bool result;
+
+	// Clear buffers to begin the screen
+	m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	// Generate the view matrix based on camera's position
+	m_Camera->Render();
+
+	// Get the world, view, and projection matrices from the camera and d3d objects.
+	m_Direct3D->GetWorldMatrix(worldMatrix);
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+	
+	scaleMatrix = XMMatrixScaling(0.1f, 0.1f, 0.1f);
+	rotateMatrix = XMMatrixRotationY(3.1);
+	srMatrix = XMMatrixMultiply(scaleMatrix, rotateMatrix);
+	worldMatrix = srMatrix;
+
+	// Put the model vertex and index buffers on the graphics pipeline to prepare them for drawing.
+	m_Model->Render(m_Direct3D->GetDeviceContext());
+
+	// Render the model using the light shader.
+	result = m_LightShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_Model->GetTexture(),
+		m_Light->GetDirection(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(),
+		m_Camera->GetPosition(), m_Light->GetSpecularColor(), m_Light->GetSpecularPower());
+	if (!result) return false;
+
+
+	m_Direct3D->EndScene();
+	return true;
+}
+
